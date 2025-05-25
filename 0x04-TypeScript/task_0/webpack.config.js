5e715353c94b95e2d08a593d96b6c626bd62a9e0
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-05-25 13:03:18 -0700

    add main.ts

---
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
+};
