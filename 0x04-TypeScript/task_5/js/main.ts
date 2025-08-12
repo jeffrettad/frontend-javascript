680ec83404dad9081872b8000a452f37b33cdefd
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-08-12 05:09:45 -1200

    add task_5

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,23 +1,24 @@
-// Interface for MajorCredits with branding
-export interface MajorCredits {
-  _majorCreditBrand: void;
+
+interface MajorCredits {
   credits: number;
+  brand: 'MajorCredits';
 }
 
-// Interface for MinorCredits with branding
-export interface MajorCredits {
-  _majorCreditBrand: void;
+interface MinorCredits {
   credits: number;
+  brand: 'MinorCredits';
 }
 
-// Function to sum MajorCredits
-export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return { credits: subject1.credits + subject2.credits } as MajorCredits;
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'MajorCredits'
+  };
 }
 
-// Function to sum MinorCredits
-export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return { credits: subject1.credits + subject2.credits } as MinorCredits;
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'MinorCredits'
+  };
 }
-
-// Export for usage/testing
